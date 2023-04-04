fb6dc09f8d1fdb3d76db23e3bb34857e0ce004f4
Caldeen <jaro.tren6@gmail.com>
2023-04-05 00:34:35 +0200

    reactive docs layout

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,24 +1,42 @@
-import { Box, Container, Drawer, Menu, MenuItem, MenuList, Toolbar, Typography } from "@mui/material"
+import { Box, Container, Drawer, Grid, Link, List, ListItem, MenuItem, MenuList, Paper, Toolbar, Typography } from "@mui/material"
 
 const Docs = () => {
-
     return (
-        <Box id='test' sx={{ display: 'flex',justifyItems:'start' }}>
-            <Box flex={1}>
-                <Drawer variant='permanent' >
-                    <Toolbar/>
-                    <MenuList>
-                        <MenuItem>Overview</MenuItem>
-                        <MenuItem>Authentication</MenuItem>
-                        <MenuItem>Endpoints</MenuItem>
-                        <MenuItem>Errors</MenuItem>
-                    </MenuList>
-                </Drawer>
-            </Box>
-            <Container sx={{flex:4}}>
-                <Typography variant='h3'>API Docs</Typography>
-            </Container>
-        </Box>
+                <Grid container justifyContent={'space-around'}>
+                <Grid item xs minWidth={'150px'}>
+                    <Drawer variant='permanent' color="primary" >
+                        <Toolbar />
+                        <Paper sx={{ height: "100%" }}>
+                            <MenuList >
+                                <MenuItem>Overview</MenuItem>
+                                <MenuItem>Authentication</MenuItem>
+                                <MenuItem>Endpoints</MenuItem>
+                                <MenuItem>Errors</MenuItem>
+                        </MenuList>
+                        </Paper>
+                    </Drawer>
+                </Grid>
+                    
+                    <Grid item xs={6} sm={8} md={9} lg={9} xl={9}  >
+                        <Typography variant='h4'>API Docs</Typography>
+                        <Typography id="login" >Login</Typography>
+                    </Grid>
+                    <Grid item xs >
+                        <Drawer variant='permanent' anchor="right" >
+                            <Toolbar />
+                                <List >
+                                    <ListItem>Found here:</ListItem>
+                                    <ListItem>
+                                        <Link underline="hover" color='inherit' href="#login"><span>- Login</span></Link>
+                                    </ListItem>
+                                    <ListItem sx={{padding:'2px'}}>
+                                        <Link padding={0} underline="hover" color='inherit' href="#register"><span>- Register</span></Link>
+                                    </ListItem>
+                                </List>
+                        </Drawer>
+                    </Grid>
+
+        </Grid>
     )
 }
 
